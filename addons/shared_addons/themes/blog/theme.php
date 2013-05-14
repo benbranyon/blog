cadddb75152a80576c32336e6de1d3bc99f35d58
= <ben_branyon@yahoo.com>
2013-05-14 17:33:12 -0600

    new themes added

---
+++ addons/shared_addons/themes/blog/theme.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Blog extends Theme
+{
+	public $name = 'Blog';
+	public $author = 'Ben Branyon';
+	public $author_website = 'http://benbranyon.com';
+	public $website = 'http://benbranyon.com';
+	public $description = 'An HTML5 responsive template for my blog.';
+	public $version = '1.0.0';
+}
---
+++ addons/shared_addons/themes/bootstrap/theme.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Bootstrap extends Theme
+{
+	public $name = 'Bootstrap';
+	public $author = 'Ben Branyon';
+	public $author_website = 'http://benbranyon.com';
+	public $website = 'http://benbranyon.com';
+	public $description = 'An theme based on twitter bootstrap';
+	public $version = '1.0.0';
+}
