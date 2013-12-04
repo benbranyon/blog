0a70a105b9ffd4cf069e49c6e173249a4157646f
= <ben_branyon@yahoo.com>
2013-12-03 20:46:01 -0700

    updated gitignore file

---
+++ addons/shared_addons/themes/bootstraptheme/theme.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Theme_Bootstraptheme extends Theme
+{
+  public $version		= '1.0';
+
+  public $name			= 'bootstraptheme';
+  public $author		= 'Ben Branyon';
+  public $author_website	= 'http://benbranyon.com';
+  public $description	= 'Bootswatch HTML responsive template, based on Bootstrap.';
+  public $website		= 'http://benbranyon.com';
+  public $options 		= array(
+		'show_breadcrumbs' 	=> array(
+			'title'         => 'Do you want to show breadcrumbs?',
+			'description'   => 'If selected it shows a string of breadcrumbs at the top of the page.',
+			'default'       => 'yes',
+			'type'          => 'radio',
+			'options'       => 'yes=Yes|no=No',
+			'is_required'   => TRUE
+		),
+	);
+}
